03bf68d8aef4a23ed72c18c3d01efe7fb2f62449
adybhav <abhavnan@buffalo.edu>
2018-03-04 22:20:45 -0500

    module 1 complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY='AIzaSyDGSHj-Y1RNjm9sW8Pn3PnnZHIA9jEsvJ0';
 
 
-const App= function(){
-  return <div>Hi!</div>;
+
+const App= () => {
+
+  return (
+    <div>
+  <SearchBar />
+  </div>
+);
 }
 
 
