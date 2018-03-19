8082a269dbb59f186f258604fa6dde93021024ca
adybhav <abhavnan@buffalo.edu>
2018-03-18 20:40:31 -0400

    lecture 31

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const VideoListItem = (props) =>{
-	const video=props.video;
+const VideoListItem = ({video, onVideoSelect}) =>{
 	const imageUrl=video.snippet.thumbnails.default.url;
-	return (<li className="list-group-item"><div className="video-list media"><div className="media-left">
+	return (<li onClick={()=> onVideoSelect(video)}
+	 className="list-group-item"><div className="video-list media"><div className="media-left">
 				<img className="media-object" src={imageUrl}/>
 		</div>
 		<div className="media-body">
